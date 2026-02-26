a15b50dcffe838e46b2f280f6fdab6ab6a1a7acc
Encore Huang <u8524154@msn.com>
2026-02-26 14:27:35 +0800

    change the prompt

--- taskpane.js
+++ taskpane.js
@@ -85,7 +85,7 @@ function initApp() {
     document.getElementById("run").innerText = isComposeMode ? "Suggest Reply" : "Summarize & Translate";
 
     document.getElementById("run").onclick = () => {
-        const prompt = isComposeMode ? "Follow the instructions or suggest a reply:" : "Summarize this and translate to Traditional Chinese:";
+        const prompt = isComposeMode ? "Follow the instructions to suggest the mail:" : "Summarize this and translate to Traditional Chinese:";
         startAI(prompt);
     };
 
